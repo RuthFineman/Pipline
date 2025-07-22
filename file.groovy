1b8ef3e96fbeae15eadcbf7edb03e1c7ddc9c1ad
RuthFineman <ruti8588@gmail.com>
2025-07-23 02:36:00 +0300

    Update file.groovy

--- file.groovy
+++ file.groovy
@@ -1,38 +1,60 @@
-pipeline{
-    agent{
-        label 'verisoft-2'
-    }
-    environment
-    {
-        PROJECT_NAME:'pipline'
-    }
-    parameters{
+pipeline {
+    agent {label 'verisoft-2'}
+
+    parameters {
         string(name: 'REPO_URL', defaultValue: 'https://github.com/RuthFineman/Pipline', description: 'Repository URL')
         string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch name to build')
-        // booleanParam (name: 'ToDoStage' defaultValue:false)
-        // string( name: 'myPhone',defaultValue: '0556556565')
     }
-    options{
-        skipStagesAfterUnstable()
-        disableConcurrentBuild()
+
+    environment {
+        MAIN_BRANCH = 'main'
     }
-    stages{
-        stage('First Stage'){
-when{
-    expression{params.ToDoStage }
-}
-steps{
-    echo 'cc'
-}
-        }
-        stage('second stage')
-        {
+
+    stages {
+        stage('Clone code') {
             steps {
-            echo "${params.myPhone}"
+                script {
+                    if (params.BRANCH_NAME == env.MAIN_BRANCH) {
+                        checkout scm
+                    } else {
+                        git branch: "${params.BRANCH_NAME}", url: "${params.REPO_URL}"
+                    }
+                }
+            }
+        }
+
+        stage('Compilation') {
+            steps {
+                echo 'Starting compilation stage'
+                timeout(time: 5, unit: 'MINUTES') {
+                    sh returnStatus:true,script:'mvn compile'
+                }
+                echo 'Compilation stage completed successfully'
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                echo 'Starting test stage'
+                timeout(time: 5, unit: 'MINUTES') {
+                    sh returnStatus:true,script:'mvn test'
+                }
+                echo 'Test stage completed successfully'
             }
         }
     }
-        triggers {
+
+    post {
+        success {
+            echo 'Pipeline completed successfully!'
+        }
+        failure {
+            echo 'Pipeline failed.'
+        }
+    }
+
+ triggers {
      cron('30 5 * * 1\n0 14 * * *')
  }
+
 }
